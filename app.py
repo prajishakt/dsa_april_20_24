40187b5a4221dbfbfecf91883043e20276278491
prajishakt <57743189+prajishakt@users.noreply.github.com>
2024-04-20 05:52:50 +0000

    1

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request
+import pickle
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/prediction',methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        height = request.form['height']
+        print(height)
+        model = pickle.load(open('model.pkl','rb'))
+        weight = model.predict([[float(height)]])
+        print(weight[0])
+    return render_template('prediction.html',weight=round(weight[0],2))
+
+if __name__ =='__main__':
+    app.run()
+
